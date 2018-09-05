76e94030fb68bc46609a3ef360c83de654c9b9d4
Marc Raftesath (Manpower SA (Pty) Ltd) <Marcraffy@gmail.com>
2018-09-05 14:30:41 +0200

    Init Sync

---
+++ SyncHub.cs
@@ -0,0 +1,39 @@
+﻿using Abp.AppFactory.Interfaces;
+using Abp.AspNetCore.SignalR.Hubs;
+using Abp.Auditing;
+using Abp.RealTime;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace Abp.AppFactory.Sync
+{
+    public class SyncHub : AbpCommonHub, ISyncHub
+    {
+        private readonly IHubContext<SyncHub> context;
+
+        public SyncHub(IOnlineClientManager onlineClientManager,
+            IClientInfoProvider clientInfoProvider,
+            IHubContext<SyncHub> context) : base(onlineClientManager, clientInfoProvider)
+        {
+            this.context = context;
+        }
+
+        public async Task Sync(Type entityType)
+        {
+            await context.Clients.All.SendCoreAsync(entityType.Name, new object[0]);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "Users");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "Users");
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
---
+++ SyncModule.cs
@@ -0,0 +1,20 @@
+﻿using Abp.AppFactory.Interfaces;
+using Abp.Dependency;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+
+namespace Abp.AppFactory.Sync
+{
+    public class SyncModule : AbpModule
+    {
+        public override void PreInitialize()
+        {
+            IocManager.Register<ISyncHub, SyncHub>(DependencyLifeStyle.Transient);
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(typeof(SyncModule).GetAssembly());
+        }
+    }
+}
